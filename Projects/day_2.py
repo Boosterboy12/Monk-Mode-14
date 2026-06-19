faeb38307823b02c26a5befec4557870d13b3e2e
Vihaan Singh <boosterboy2030@gmail.com>
2026-06-19 17:12:15 +0530

    Updated The Day-2 Project File

--- Projects/day_2.py
+++ Projects/day_2.py
@@ -1,5 +1,19 @@
 class emplyee:
-    def __init__(self, salary = 1000, name, age):
+
+    def __init__(self, name, age, salary=1000):
         self.name = name 
         self.salary = salary
         self.age = age
+
+    def hire(name, age):
+        try:
+            attendee_name = str(input("Enter Your Name - ")).lower()
+            attendee_age = int(input("Enter Your Age - "))
+
+        except:
+            print("Please Enter A Valid Input !")
+        
+        finally:
+            print("Glad You Choose To Invest Your Precious Time With Us !")
+
+new_emplyee = emplyee.hire("Vihaan", "shs")
