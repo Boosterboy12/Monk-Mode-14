9502ee04b9d1dd363a7434dca821160cc9bf1680
Vihaan Singh <boosterboy2030@gmail.com>
2026-06-19 17:19:10 +0530

    Made Some Changes In The Day_2 File

--- Projects/day_2.py
+++ Projects/day_2.py
@@ -5,15 +5,20 @@ class emplyee:
         self.salary = salary
         self.age = age
 
-    def hire(name, age):
+    def hire( name, age):
         try:
-            attendee_name = str(input("Enter Your Name - ")).lower()
+            attendee_name = input("Enter Your Name - ").lower()
             attendee_age = int(input("Enter Your Age - "))
+
+            if name == int:
+                print("Please Enter A Valid String Name")
 
         except:
             print("Please Enter A Valid Input !")
-        
+
+        else:
+            print(f"Well Done {name} You Have Been Selected For The Job")
         finally:
             print("Glad You Choose To Invest Your Precious Time With Us !")
 
-new_emplyee = emplyee.hire("Vihaan", "shs")
+new_emplyee = emplyee.hire("Vihaan", 12)
