4b71aa0c428d05e42031bccce779dc510d333207
Vihaan Singh <boosterboy2030@gmail.com>
2026-06-19 11:04:53 +0530

    Completed The Day-2 Question

--- practice/day_2.py
+++ practice/day_2.py
@@ -1,14 +1,23 @@
+# Create a Python class called Coffee that displays a menu of drinks and allows the user to place an order. The program should:
+# Show at least three drink options.
+# Take the user's order as input.
+# Accept inputs regardless of letter case.
+# Display a confirmation message for valid orders.
+# Display an error message for invalid orders.
+# Thank the customer before the program ends.
+
+# --- Solution --- #
 class Cofee:
     def order(self):
         print("1 = Chai - $20, 2 = Cold Coffee - $30, 3 = Hot Cofee - $30")
-        a = input("Select Your Order : ")
-        if a == "Chai":
+        a = input("Select Your Order : ",).lower()
+        if a == "chai":
             print("Thanks ! Your Chai Is Ready")
 
-        elif a == "Cold Coffee":
+        elif a == "cold coffee":
             print("Thanks ! Your Cold Cofee Is Ready")
 
-        elif a == "Hot Coffee":
+        elif a == "hot coffee":
             print("Thanks ! Your Hot Cofee Is Ready")
 
         else:
