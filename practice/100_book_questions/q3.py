ff83bd1dd415ca1e3a213ec34fd52615c78875e9
Vihaan Singh <boosterboy2030@gmail.com>
2026-06-19 23:06:12 +0530

    Completed The q3 Temperature Converter File

---
+++ practice/100_book_questions/q3.py
@@ -0,0 +1,9 @@
+# Temperature Converter
+class temperature_converter:
+    def __init__(self, celcius, kelvin, fahrenheit):
+        self.celcius = celcius
+        self.kelvin = kelvin
+        self.fahrenheit = fahrenheit
+
+    def celcius_to_fahrenheit(celcius):
+        
