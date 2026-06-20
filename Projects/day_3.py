be45963d58da64822ede2de32f9f64b4ce7d98e4
Vihaan Singh <boosterboy2030@gmail.com>
2026-06-20 18:18:11 +0530

    Fnished Day 3 project file

--- Projects/day_3.py
+++ Projects/day_3.py
@@ -1,8 +1,11 @@
 class identity:
     def __init__(self):
+        pass
+
+    def greet(name, age, profession):
         name = input("Enter You Name : ")
         age = int(input("Enter Your Age : "))
         profession = input("Enter The Profession You Love FROM THE FOLLOWING : 1.Coder, 2. Developer, 3. Tech Enthusiast - ")
-
-    def greet(name, age, profession):
         print(f"Welcome {name} as you are a {profession} we would like you to answer the follwoing questions")
+
+vihaan = identity.greet("Vihaan", 13, "Coder" )
